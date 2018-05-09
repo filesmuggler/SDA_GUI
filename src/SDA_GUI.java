98eb47b4fed76a80190c8d8cfb8f8fd212bc5116
Krzysztof Stężała <krzste09@outlook.com>
2018-05-09 20:54:31 +0200

    added checkboxes

--- src/SDA_GUI.java
+++ src/SDA_GUI.java
@@ -17,12 +17,15 @@ limitations under the License.
 */
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -49,21 +52,36 @@ public class SDA_GUI extends Application {
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
 
-        Text scenetitle = new Text("Welcome");
-        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        grid.add(scenetitle, 0, 0, 2, 1);
+        //Text scenetitle = new Text("Welcome");
+        //scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        //grid.add(scenetitle, 0, 0, 2, 1);
 
-        Label userName = new Label("User Name:");
-        grid.add(userName, 0, 1);
+        //display checkboxes to select if divide or merge
+        //generate proper textfields and labels
+        
+        CheckBox if_divide = new CheckBox("Divide");
+        grid.add(if_divide,0,1);
+        CheckBox if_merge = new CheckBox("Merge");
+        grid.add(if_merge,1,1);
+        CheckBox if_encrypt = new CheckBox("Encrypt");
+        grid.add(if_encrypt,0,2);
+        CheckBox if_decrypt = new CheckBox("Decrypt");
+        grid.add(if_decrypt,1,2);
+        //CheckBox if_plain = new CheckBox("No encryption");
+        //grid.add(if_plain,1,3);
+        
+        
+        //Label userName = new Label("User Name:");
+        //grid.add(userName, 0, 1);
 
-        TextField userTextField = new TextField();
-        grid.add(userTextField, 1, 1);
+        //TextField userTextField = new TextField();
+        //grid.add(userTextField, 1, 1);
 
-        Label pw = new Label("Password:");
-        grid.add(pw, 0, 2);
+        //Label pw = new Label("Password:");
+        //grid.add(pw, 0, 2);
 
-        PasswordField pwBox = new PasswordField();
-        grid.add(pwBox, 1, 2);
+        //PasswordField pwBox = new PasswordField();
+        //grid.add(pwBox, 1, 2);
 
         Button btn = new Button("Sign in");
         HBox hbBtn = new HBox(10);
@@ -82,6 +100,51 @@ public class SDA_GUI extends Application {
                 actiontarget.setText("Sign in button pressed");
             }
         });
+        
+        if_divide.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+            	if_merge.setSelected(!newValue);
+            	if_decrypt.setSelected(false);
+            }
+        });
+
+        if_merge.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+            	if_divide.setSelected(!newValue);
+            	if_encrypt.setSelected(false);
+            }
+        });
+        
+        if_encrypt.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+            	if_decrypt.setSelected(false);
+            	//if_plain.setSelected(!newValue);
+            	if_merge.setSelected(false);
+            }
+        });
+
+        if_decrypt.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+            	if_encrypt.setSelected(false);
+            	//if_plain.setSelected(!newValue);
+            	if_divide.setSelected(false);
+            }
+        });
+        /*
+        if_plain.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+            	if_encrypt.setSelected(false);
+            	if_decrypt.setSelected(false);
+            	//if_divide.setSelected(!newValue);
+            }
+        });*/
+        
+        
 
         Scene scene = new Scene(grid, 300, 275);
         primaryStage.setScene(scene);
