546deebe7b3cf6aeb02db1851820936f4729fc89
Krzysztof Stężała <krzste09@outlook.com>
2018-05-10 16:46:33 +0200

    gui updated

--- src/SDA_GUI.java
+++ src/SDA_GUI.java
@@ -40,6 +40,9 @@ import javafx.stage.Stage;
 
 public class SDA_GUI extends Application {
     
+	private boolean if_success;
+	
+	
     public static void main(String[] args) {
         launch(args);
     }
@@ -48,7 +51,7 @@ public class SDA_GUI extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("SDA_GUI (beta)");
         GridPane grid = new GridPane();
-        grid.setAlignment(Pos.TOP_LEFT);
+        grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
@@ -62,58 +65,100 @@ public class SDA_GUI extends Application {
         CheckBox if_decrypt = new CheckBox("Decrypt");
         
         HBox OperationButtons = new HBox(5);
-        OperationButtons.setSpacing(10);
+        OperationButtons.setSpacing(50);
         OperationButtons.getChildren().addAll(if_divide, if_merge);
         grid.add(OperationButtons, 0, 1);
         
         HBox EncryptionButtons = new HBox(5);
-        EncryptionButtons.setSpacing(10);
+        EncryptionButtons.setSpacing(50);
         EncryptionButtons.getChildren().addAll(if_encrypt, if_decrypt);
         grid.add(EncryptionButtons, 0, 2);
         
-        //Label userName = new Label("User Name:");
-        //grid.add(userName, 0, 1);
+        
+        Label d_inputfile = new Label("Input File:");        
+        Label d_outputfile_1 = new Label("Output File 1:");        
+        Label d_outputfile_2 = new Label("Output File 2:");
+        
+        TextField d_inputfile_f = new TextField();
+        TextField d_outputfile_1_f = new TextField();
+        TextField d_outputfile_2_f = new TextField();
 
-        //TextField userTextField = new TextField();
-        //grid.add(userTextField, 1, 1);
-
-        //Label pw = new Label("Password:");
-        //grid.add(pw, 0, 2);
-
-        //PasswordField pwBox = new PasswordField();
-        //grid.add(pwBox, 1, 2);
-
-        Button btn = new Button("Next");
+        Label m_inputfile_1 = new Label("Input File 1:");        
+        Label m_inputfile_2 = new Label("Input File 2:");
+        Label m_outputfile = new Label("Output File:");
+        
+        TextField m_inputfile_1_f = new TextField();
+        TextField m_inputfile_2_f = new TextField();
+        TextField m_outputfile_f = new TextField();
+        
+        
+        //TODO define the children on DivideFields
+        VBox DivideLabels = new VBox();
+        DivideLabels.setSpacing(10);
+        DivideLabels.getChildren().addAll(d_inputfile, d_outputfile_1, d_outputfile_2);
+        grid.add(DivideLabels, 0, 3);
+        DivideLabels.setVisible(false);
+        
+        //TODO define the children on MergeFields
+        VBox MergeLabels = new VBox();
+        MergeLabels.setSpacing(10);
+        MergeLabels.getChildren().addAll(m_inputfile_1,m_inputfile_2,m_outputfile);
+        grid.add(MergeLabels, 0, 3);
+        MergeLabels.setVisible(false);
+        
+        //TODO define the children on DivideFields
+        VBox DivideFields = new VBox();
+        DivideFields.setSpacing(10);
+        DivideFields.getChildren().addAll(d_inputfile_f, d_outputfile_1_f, d_outputfile_2_f);
+        grid.add(DivideFields, 1, 3);
+        DivideFields.setVisible(false);
+        
+        //TODO define the children on MergeFields
+        VBox MergeFields = new VBox();
+        MergeFields.setSpacing(10);
+        MergeFields.getChildren().addAll(m_inputfile_1_f,m_inputfile_2_f,m_outputfile_f);
+        grid.add(MergeFields, 1, 3);
+        MergeFields.setVisible(false);
+        
+        Button btn = new Button("Execute");
         HBox hbBtn = new HBox(10);
         hbBtn.setAlignment(Pos.BOTTOM_LEFT);
         hbBtn.getChildren().add(btn);
-        grid.add(hbBtn, 0, 3);
+        grid.add(hbBtn, 1, 6);
 
         final Text actiontarget = new Text();
-        grid.add(actiontarget, 0, 4);
+        grid.add(actiontarget, 0, 6);
 
         btn.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent e) {
                 actiontarget.setFill(Color.FIREBRICK);
-                actiontarget.setText("Sign in button pressed");
+                actiontarget.setText("Output of the operation");
             }
         });
         
         if_divide.selectedProperty().addListener(new ChangeListener<Boolean>() {
             @Override
             public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-            	if_merge.setSelected(!newValue);
+            	if_merge.setSelected(false);
             	if_decrypt.setSelected(false);
+            	MergeFields.setVisible(false);
+            	DivideLabels.setVisible(true);
+            	MergeLabels.setVisible(false);
+            	DivideFields.setVisible(true);
             }
         });
 
         if_merge.selectedProperty().addListener(new ChangeListener<Boolean>() {
             @Override
             public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-            	if_divide.setSelected(!newValue);
+            	if_divide.setSelected(false);
             	if_encrypt.setSelected(false);
+            	MergeFields.setVisible(true);
+            	DivideLabels.setVisible(false);
+            	MergeLabels.setVisible(true);
+            	DivideFields.setVisible(false);
             }
         });
         
@@ -136,7 +181,7 @@ public class SDA_GUI extends Application {
         
         
 
-        Scene scene = new Scene(grid, 300, 275);
+        Scene scene = new Scene(grid, 400, 300);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
